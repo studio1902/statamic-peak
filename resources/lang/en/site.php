5b107433730ef182243f5c6e932be16f32af0b66
Rob de Kort <rob@studio1902.nl>
2020-08-07 14:42:52 +0200

    Add string translation file for templates

---
+++ resources/lang/en/site.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    // Pagination
+    'previous'                          => 'previous',
+    'of'                                => 'of',
+    'next'                              => 'next',
+];
