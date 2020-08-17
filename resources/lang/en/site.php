b598338d04e52b84532ad45e54822beea05e2653
Rob de Kort <rob@studio1902.nl>
2020-08-17 14:21:04 +0200

    Add contact form

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -4,4 +4,19 @@ return [
     'previous'                          => 'previous',
     'of'                                => 'of',
     'next'                              => 'next',
+
+    // Contact form
+    'form_name'                         => 'Name',
+    'form_email'                        => 'E-mail address',
+    'form_phone_number'                 => 'Phone number',
+    'form_fax'                          => 'Fax',
+    'form_message'                      => 'Message',
+    'form_send'                         => 'Send message',
+    'form_success'                      => 'Thank you, we received your message and will contact you as soon as possible.',
+    'form_mail_title'                   => 'Contact form',
+    'form_mail_greeting'                => 'Dear',
+    'form_mail_body_sender'             => 'Thanks for your message. We will contact you as soon as possible.',
+    'form_mail_body_owner'              => 'A contact form has been sent.',
+    'form_mail_closing'                 => 'Kind regards',
+    'form_mail_from'                    => 'waltec.info',
 ];
