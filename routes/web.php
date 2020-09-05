8ba79b2e533a87e741b4e4a7dc164eea5752d45a
Rob de Kort <rob@studio1902.nl>
2020-09-05 14:46:34 +0200

    Add sitemap.xml, focal crop og images

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 // ]);
 
 Route::get('/!/DynamicToken/refresh', 'DynamicToken@getRefresh');
+Route::statamic('/sitemap.xml', 'sitemap/sitemap', ['layout' => null, 'content_type' => 'application/xml']); 
