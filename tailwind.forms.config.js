db887fa53711e0f10bca4d0a645b66ef6484ab0b
Rob de Kort <rob@studio1902.nl>
2020-10-24 12:32:20 +0200

    WIP Separate config files

---
+++ tailwind.forms.config.js
@@ -0,0 +1,31 @@
+//--------------------------------------------------------------------------
+// Tailwind Custom Forms configuration
+//--------------------------------------------------------------------------
+//
+// Here you may overwrite the default Tailwind Custom Forms styles.
+// Some defaults are provided.
+// More info: https://github.com/tailwindlabs/tailwindcss-custom-forms.
+//
+
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  theme: {
+    customForms: theme => ({
+      default: {
+        input: {
+          borderColor: theme('colors.neutral.300'),
+          color: theme('colors.neutral.800'),
+        },
+      },
+      error: {
+        'input, textarea': {
+          borderColor: theme('colors.error.700'),
+        },
+      },
+    })
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
+}
