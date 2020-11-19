903901535d251576344e063f4c31f11d5ddb0f52
Rob de Kort <rob@studio1902.nl>
2020-11-19 09:25:30 +0100

    Import colors

--- tailwind.config.site.js
+++ tailwind.config.site.js
@@ -8,6 +8,7 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   theme: {
