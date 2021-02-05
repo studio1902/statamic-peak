a9613eeb7a76a7e8f83f490cb7fc47c5885a7287
Rob de Kort <rob@studio1902.nl>
2021-02-05 17:59:53 +0100

    Add resolution, remove svg hack

--- app/Listeners/GenerateFavicons.php
+++ app/Listeners/GenerateFavicons.php
@@ -47,19 +47,15 @@ class GenerateFavicons implements ShouldQueue
     {
         try {
             $im = new \Imagick();
+            $im->setResolution($width, $height);
             $im->setBackgroundColor(new \ImagickPixel($background));
-
             $svgdata = file_get_contents($import);
-            $svgdata = $this->svgScaleHack($svgdata, $width, $height);
-
             $im->readImageBlob($svgdata);
             if ($border)
                 $im->borderImage($background, $border, $border);
             $im->resizeImage($width, $height, \imagick::FILTER_LANCZOS, 1);
-            
             $im->setImageFormat('png32');
             file_put_contents($export, $im->getImageBlob());
-
             $im->clear();
             $im->destroy();
             return true;
@@ -67,31 +63,5 @@ class GenerateFavicons implements ShouldQueue
         catch(Exception $e) {
             return false;
         }
-    }
-
-    private function svgScaleHack($svg, $minWidth, $minHeight)
-    {
-        $reW = '/(.*<svg[^>]* width=")([\d.]+px)(.*)/si';
-        $reH = '/(.*<svg[^>]* height=")([\d.]+px)(.*)/si';
-        preg_match($reW, $svg, $mw);
-        preg_match($reH, $svg, $mh);
-        $width = floatval($mw[2]);
-        $height = floatval($mh[2]);
-        if (!$width || !$height) return false;
-
-        // scale to make width and height big enough
-        $scale = 1;
-        if ($width < $minWidth)
-            $scale = $minWidth/$width;
-        if ($height < $minHeight)
-            $scale = max($scale, ($minHeight/$height));
-
-        $width *= $scale*2;
-        $height *= $scale*2;
-
-        $svg = preg_replace($reW, "\${1}{$width}px\${3}", $svg);
-        $svg = preg_replace($reH, "\${1}{$height}px\${3}", $svg);
-
-        return $svg;
     }
 }
