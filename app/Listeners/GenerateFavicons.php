38d115e2a735230eaa69a79ea8bdd1dfed1048c0
Rob de Kort <rob@studio1902.nl>
2021-02-06 16:10:20 +0100

    Rewrite and improve favicon generation

--- app/Listeners/GenerateFavicons.php
+++ app/Listeners/GenerateFavicons.php
@@ -44,29 +44,40 @@ class GenerateFavicons implements ShouldQueue
     private function createThumbnail($import, $export, $width, $height, $background, $border)
     {
         $svg = file_get_contents($import);
-        $im = new \Imagick();
-        $im->setResolution($width, $height);
-        $im->setBackgroundColor(new \ImagickPixel($background));
-        $im->readImageBlob($svg);
         $svgObj = simplexml_load_string($svg);
         $viewBox = explode(' ', $svgObj['viewBox']);
         $viewBoxWidth = $viewBox[2];
         $viewBoxHeight = $viewBox[3];
-        $viewBoxWidth < $viewBoxHeight 
-            ? $im->resizeImage($width, $height, \imagick::FILTER_LANCZOS, 1) 
-            : $im->resizeImage($width, $height, \imagick::FILTER_LANCZOS, 1);
+        if ($viewBoxWidth >= $viewBoxHeight) {
+            $ratio = $width / $viewBoxWidth;
+        } else {
+            $ratio = $height / $viewBoxHeight;
+        }
 
-        // if ($border) $im->borderImage($background, $border, $border);
-        // $im->resizeImage($width, $height, \imagick::FILTER_LANCZOS, 1);
-        // $im->extentImage($width * 2, $height * 2, 0, 0);
+        $im = new \Imagick();
+        $im->setResolution($viewBoxWidth * $ratio * 2, $viewBoxHeight * $ratio * 2);
+        $im->setBackgroundColor(new \ImagickPixel($background));
+        $im->readImageBlob($svg);
+        $im->resizeImage($viewBoxWidth * $ratio, $viewBoxHeight * $ratio, \imagick::FILTER_LANCZOS, 1);
+
+        if ($viewBoxWidth >= $viewBoxHeight) {
+            $compensateHeight = $height - ($viewBoxHeight * $ratio);
+            $im->extentImage($width, $height - $compensateHeight / 2, 0, $compensateHeight * -.5);
+            $im->extentImage($width, $height, 0, 0);
+        } else {
+            $compensateWidth = $width - ($viewBoxWidth * $ratio);
+            $im->extentImage($width - $compensateWidth / 2, $height, $compensateWidth * -.5, 0);
+            $im->extentImage($width, $height, 0, 0);
+        }
+
+        if ($border) {
+            $im->borderImage($background, $border, $border);
+            $im->resizeImage($width, $height, \imagick::FILTER_LANCZOS, 1);
+        }
 
         $im->setImageFormat('png32');
         file_put_contents($export, $im->getImageBlob());
         $im->clear();
         $im->destroy();
-        return true;
     }
 }
-
-// if ($border) $im->borderImage($background, $border, $border);
-// $im->resizeImage($width, $height, \imagick::FILTER_LANCZOS, 1);
