f4090522e0f3c83ea4d655880efa87aaf047a83a
Rob de Kort <rob@studio1902.nl>
2021-05-11 17:40:37 +0200

    Test for instance and collection

--- app/Actions/GenerateSocialImages.php
+++ app/Actions/GenerateSocialImages.php
@@ -8,24 +8,22 @@ use Illuminate\Support\Str;
 use Spatie\Browsershot\Browsershot;
 use Statamic\Actions\Action;
 use Statamic\Facades\Entry;
+use Statamic\Contracts\Entries\Entry as EntryInstance;
 
 class GenerateSocialImages extends Action implements ShouldQueue
 {
-    // public function visibleTo($item)
-    // {
-    //     return $item instanceof Entry;
-    // }
+    public $available_collections = array('pages');
+
+    public function visibleTo($item)
+    {
+        return $item instanceof EntryInstance && in_array($item->collectionHandle(), $this->available_collections);
+    }
 
     public function authorize($user, $item)
     {
         return $user->can('edit', $item);
     }
 
-    /**
-     * The run method
-     *
-     * @return void
-     */
     public function run($items, $values)
     {
         $items->each(function($item, $key) {
--- content/collections/pages/home.md
+++ content/collections/pages/home.md
@@ -2,7 +2,7 @@
 id: home
 title: Home
 updated_by: 0099140b-7e75-4f28-a8f7-9ff4fec4e3a9
-updated_at: 1620730756
+updated_at: 1620746956
 seo_noindex: false
 seo_nofollow: false
 seo_canonical_type: entry
@@ -11,5 +11,4 @@ sitemap_priority: 0.5
 og_description: 'Statamic Peak is an opinionated starter kit for all your Statamic sites. It''s design agnostic but comes bundled with tools like Tailwind and AlpineJS and a workflow you can use to build anything you want.'
 og_render_image: false
 og_title: 'Statamic Peak'
-og_image: home-twitter.png
 ---
