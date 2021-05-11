0093e51689f993cc690dbf6acdf90a33ef36a7e5
Rob de Kort <rob@studio1902.nl>
2021-05-11 20:34:32 +0200

    Finish first version

--- app/Actions/GenerateSocialImages.php
+++ app/Actions/GenerateSocialImages.php
@@ -7,23 +7,30 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Str;
 use Spatie\Browsershot\Browsershot;
 use Statamic\Actions\Action;
-use Statamic\Facades\Entry;
 use Statamic\Contracts\Entries\Entry as EntryInstance;
+use Statamic\Facades\Entry;
+use Statamic\Globals\GlobalSet;
 
 class GenerateSocialImages extends Action implements ShouldQueue
 {
-    public $available_collections = array('pages');
+    public $available_collections = array();
 
+    public function __construct() {
+        if (GlobalSet::findByHandle('seo')->inDefaultSite()->get('use_social_image_generation') && GlobalSet::findByHandle('seo')->inDefaultSite()->get('social_images_collections')) {
+            $this->available_collections = GlobalSet::findByHandle('seo')->inDefaultSite()->get('social_images_collections');
+        }
+    }
+    
     public function visibleTo($item)
     {
         return $item instanceof EntryInstance && in_array($item->collectionHandle(), $this->available_collections);
     }
-
+    
     public function authorize($user, $item)
     {
         return $user->can('edit', $item);
     }
-
+    
     public function run($items, $values)
     {
         $items->each(function($item, $key) {
@@ -31,17 +38,21 @@ class GenerateSocialImages extends Action implements ShouldQueue
             $title = Str::of($item->get('title'))->slug('-');
             $app_url = config('app.url');
             
+            // Generate, save and set default og image.
             $file = "{$title}-og.png";
             $image = Browsershot::url("{$app_url}/social-images/{$id}")
                 ->windowSize(1200, 630)
                 ->select('#og')
-                ->save(public_path($file));
-    
+                ->save("social_images/{$file}");
+            $item->set('og_image', $file)->save();
+            
+            // Generate, save and set default twitter image.
             $file = "{$title}-twitter.png";
             $image = Browsershot::url("{$app_url}/social-images/{$id}")
                 ->windowSize(1200, 600)
                 ->select('#twitter')
-                ->save(public_path($file));
+                ->save("social_images/{$file}");
+            $item->set('twitter_image', $file)->save();
         });
     }
 }
