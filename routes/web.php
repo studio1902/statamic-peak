6817bc3318fe1071f32ed95ccacfebac0421d16e
Rob de Kort <rob@studio1902.nl>
2021-06-10 11:02:53 +0200

    Enable OG generation for server testing

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,20 @@ Route::get('/!/DynamicToken/refresh', [DynamicToken::class, 'getRefresh']);
 
 // The Sitemap route to the sitemap.xml
 Route::statamic('/sitemap.xml', 'sitemap/sitemap', [
-    'layout' => null, 
+    'layout' => null,
     'content_type' => 'application/xml'
-]); 
+]);
 
 // The Manifest route to the manifest.json
 Route::statamic('/site.webmanifest', 'manifest/manifest', [
-    'layout' => null, 
+    'layout' => null,
     'content_type' => 'application/json'
-]); 
+]);
 
 // The Social Image route to generate social images.
-// Route::statamic('/social-images/{id}', 'social_images', [
-//     'layout' => null
-// ]); 
+Route::statamic('/social-images/{id}', 'social_images', [
+    'layout' => null
+]);
 
 // The Search route to display search results with `views/search.antlers.html`.
 // Route::statamic('/search', 'search', [
