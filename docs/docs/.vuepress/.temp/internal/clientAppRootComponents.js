e5ec1f8f597f2c15d6f5d36d50e36c385514b77f
Rob de Kort <rob@studio1902.nl>
2021-09-05 22:09:24 +0200

    Untrack

--- docs/docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/robdekort/Sites/statamic-peak-docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
