27d72b67a04e47e383cd838f9753cf9aa49794a1
Rob de Kort <rob@studio1902.nl>
2021-10-26 14:07:50 +0200

    Comment social image route by default

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,11 @@ Route::statamic('/site.webmanifest', 'manifest/manifest', [
 ]);
 
 // The Social Image route to generate social images.
-Site::all()->each(function ($site) {
-    Route::statamic("{$site->url()}/social-images/{id}", 'social_images', [
-        'layout' => null,
-    ]);
-});
+// Site::all()->each(function ($site) {
+//     Route::statamic("{$site->url()}/social-images/{id}", 'social_images', [
+//         'layout' => null,
+//     ]);
+// });
 
 // The Search route to display search results with `views/search.antlers.html`.
 // Route::statamic('/search', 'search', [
