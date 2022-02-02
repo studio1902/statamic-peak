da0443e2936ac8f540b28279f484cdb8c33eb9ad
Rob de Kort <rob@studio1902.nl>
2022-02-02 09:21:37 +0100

    Update deps, remove postcss nested

--- dev/webpack.mix.js
+++ dev/webpack.mix.js
@@ -6,7 +6,6 @@ mix.js('resources/js/site.js', 'public/js/site.js')
 mix.postCss('resources/css/site.css', 'public/css/site.css', [
     require('postcss-import'),
     require('tailwindcss'),
-    require('postcss-nested'),
     require('postcss-focus-visible'),
     require('autoprefixer'),
 ])
@@ -18,8 +17,8 @@ mix.options({
 mix.browserSync({
     proxy: process.env.APP_URL,
     files: [
-        'resources/views/**/*.html', 
-        'public/**/*.(css|js)', 
+        'resources/views/**/*.html',
+        'public/**/*.(css|js)',
     ],
     // Option to open in non default OS browser.
     // browser: "firefox",
