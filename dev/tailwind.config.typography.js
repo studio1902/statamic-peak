42a200e4d5ce348bf1ce5fe42413ba4fca34826d
Rob de Kort <rob@studio1902.nl>
2022-02-22 20:04:01 +0100

    Move TW typo config to partial

--- dev/tailwind.config.typography.js
+++ dev/tailwind.config.typography.js
@@ -2,8 +2,7 @@
 // Tailwind Typography configuration
 //--------------------------------------------------------------------------
 //
-// Here you may overwrite the default Tailwind Typography (or prosé) styles.
-// Some defaults are provided.
+// Here you may overwrite the default Tailwind Typography (or prosé) styles or configure certain variants.
 // More info: https://tailwindcss.com/docs/typography-plugin.
 //
 
@@ -15,53 +14,9 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: `${theme('colors.neutral.DEFAULT')}`,
-            '[class~="lead"]': {
-              color: `${theme('colors.neutral.DEFAULT')}`,
-            },
-            a: {
-              color: `${theme('colors.primary.DEFAULT')}`,
-              '&:hover': {
-                color: `${theme('colors.primary.DEFAULT')} !important`,
-              },
-              '&.focus-visible': {
-                outline: 'none',
-                boxShadow: `0 0 0 2px ${theme('colors.primary.DEFAULT')}`,
-                borderRadius: `${theme('borderRadius.sm')}`,
-              },
-              '&:focus-visible': {
-                outline: 'none',
-                boxShadow: `0 0 0 2px ${theme('colors.primary.DEFAULT')}`,
-                borderRadius: `${theme('borderRadius.sm')}`,
-              },
-            },
-            'a.no-underline': {
-              textDecoration: 'none',
-            },
-            'h1, h2, h3, h4': {
-              color: `${theme('colors.primary.DEFAULT')}`,
-            },
-            blockquote: {
-              borderColor: `${theme('colors.primary.DEFAULT')}`,
-            },
-            hr: {
-              borderColor: `${theme('colors.neutral.DEFAULT')}`,
-            },
-            'ul > li::marker': {
-              backgroundColor: `${theme('colors.neutral.DEFAULT')}`,
-            },
-            'ol > li::marker': {
-              color: `${theme('colors.neutral.DEFAULT')}`,
-            },
             'ul > li p, ol > li p': {
               marginTop: '0em !important',
               marginBottom: '0em !important',
-            },
-            pre: {
-              whiteSpace: 'pre-wrap',
-            },
-            strong: {
-              color: `${theme('colors.neutral.DEFAULT')}`,
             },
           }
         }
