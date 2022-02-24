c64ac4ddb974aa2221d59178f2a6a0fefc6a62f1
Rob de Kort <rob@studio1902.nl>
2022-02-24 10:57:15 +0100

    Add missing alt text widget

--- dev/resources/lang/en/strings.php
+++ dev/resources/lang/en/strings.php
@@ -64,4 +64,10 @@ return [
     // Social images
     'social_images'                     => 'Generated one social image.|Generated :count social images.',
     'social_images_queue'               => 'Generating one social image in the background.|Generating :count social images in the background.',
+
+    // Widgets
+    'widget_assets_title'               => 'Assets without Alt Text',
+    'widget_assets_edit'                => 'Edit this Asset',
+    'widget_assets_explanation'         => 'It\'s important to add alt text describing your images. This helps users who depend on assistive technology. You have :amount image that need attention.|It\'s important to add alt text describing your images. This helps users who depend on assistive technology. You have :amount images that need attention.',
+    'widget_assets_done'                => 'All assets have an alt text.',
 ];
