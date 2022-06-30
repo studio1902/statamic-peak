954980cd5e53b03088ce573799da6bea0d124069
Kuldas <jsem@stepankulik.cz>
2022-06-30 17:04:32 +0200

    Czech translation

---
+++ lang/cs/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo bylo resetováno!',
+    'sent' => 'Poslali jsme vám e-mailem odkaz na obnovení hesla!',
+    'throttled' => 'Před dalším pokusem počkejte.',
+    'token' => 'Token pro resetování hesla je neplatný.',
+    'user' => "Uživatele s touto e-mailovou adresou nemůžeme najít",
+
+];
