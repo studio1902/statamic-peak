569bc1ed4bc7e4489403552a8e753bc9ad2c930b
Rob de Kort <rob@studio1902.nl>
2022-07-05 21:21:35 +0200

    Use env in vite config

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -1,26 +1,17 @@
 import laravel from 'laravel-vite-plugin'
-import {defineConfig} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-    plugins: [
-        laravel([
-            'resources/css/site.css',
-            'resources/js/site.js',
-        ]),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [
+            laravel([
+                'resources/css/site.css',
+                'resources/js/site.js',
+            ])
+        ],
+        server: {
+            open: env.APP_URL
         }
-    ],
-    server: {
-        host: 'statamic-peak.test',
-        open: '/'
-    },
+    }
 });
