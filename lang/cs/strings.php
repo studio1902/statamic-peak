07581891267b787d7e718554e175fb972aa28416
Štěpán Kulík <jsem@stepankulik.cz>
2022-07-13 20:04:22 +0200

    Adding Czech translation for Peak (#233)
    * Czech translation
    
    * Update Czech translation
    
    * Update Czech translation
    
    * Delete auth,pagination, passwords, validation

---
+++ lang/cs/strings.php
@@ -0,0 +1,82 @@
+<?php
+return [
+    // Contact form
+    'form_honeypot'                     => 'Fax',
+    'form_mail_body_owner'              => 'Kontaktní formulář byl odeslán.',
+    'form_mail_body_sender'             => 'Děkujeme za vaši zprávu. Budeme vás kontaktovat co nejdříve.',
+    'form_mail_closing'                 => 'S přátelským pozdravem',
+    'form_mail_from'                    => config('app.name'),
+    'form_mail_greeting'                => 'Vážený',
+    'form_mail_logo'                    => 'https://cdn.studio1902.nl/assets/statamic-peak/statamic-peak-logo.png',
+    'form_mail_submitted'               => 'Odesláno',
+    'form_mail_title'                   => 'Kontaktní formulář',
+    'form_mail_url'                     => config('app.url'),
+    'form_send'                         => 'Poslat zprávu',
+    'form_success'                      => 'Děkujeme, vaši zprávu jsme obdrželi a budeme vás co nejdříve kontaktovat.',
+
+    // Cookie banner
+    'cookie_title'                      => 'Cookies 🍪',
+    'cookie_functional'                 => 'Funkční',
+    'cookie_functional_explanation'     => 'Cookies potřebné pro fungování webu. Tyto nelze vypnout.',
+    'cookie_analytics'                  => 'Analytické',
+    'cookie_analytics_explanation'      => 'Third party cookies, not needed if we cared.',
+    'cookie_ads'                        => 'Reklamy & sledování',
+    'cookie_ads_explanation'            => 'Third party cookies absolutely terrible for privacy, but we don\'t give a sh*t.',
+    'cookie_accept'                     => 'Přijmout',
+    'cookie_ignore'                     => 'Ignorovat',
+    'cookie_explanation'                => 'Tento web používá cookies, které vyžadují souhlas.',
+    'cookie_learn_more'                 => 'Zjistit více',
+    'cookie_reset_consent'              => 'Resetovat cookie souhlas',
+
+    // Dark mode
+    'theme_toggle_dark'                 => 'Použít tmavý režim.',
+    'theme_toggle_light'                => 'Použít světlý režim.',
+    'theme_toggle_system'               => 'Použít systémové preference.',
+    'theme_toggle_dark_short'           => 'Tmavý',
+    'theme_toggle_light_short'          => 'Světlý',
+    'theme_toggle_system_short'         => 'Systém',
+    'theme_toggle_toggle_open'          => 'Otevřít výběr tmavého režimu.',
+    'theme_toggle_toggle_close'         => 'Zavřít výběr tmavého režimu.',
+
+    // Footer
+    'on'                                => 'on',
+
+    // Language picker
+    'language_open'                     => 'Otevřít výběr jazyka. Aktuální jazyk je :current_language',
+    'language_close'                    => 'Zavřít výběr jazyka',
+
+    // Navigation
+    'close'                             => 'Zavřít',
+    'main_navigation'                   => 'Hlavní navigace',
+    'menu'                              => 'Menu',
+    'nav_close'                         => 'Zavřít navigaci',
+    'nav_open'                          => 'Otevřít navigaci',
+    'subnav_close'                      => 'Zavřít sub navigaci',
+    'subnav_open'                       => 'Otevřít sub navigaci',
+    'skip_to_content'                   => 'Přejít na obsah',
+
+    // Other
+    'no_results'                        => 'Momentálně zde nejsou žádné příspěvky',
+    'no_script'                         => 'Tato webová stránka vyžaduje pro správnou funkci některých částí Javascript. Váš uživatelský požitek se může lišit.',
+
+    // Pagination
+    'next'                              => 'další',
+    'of'                                => 'z',
+    'previous'                          => 'předchozí',
+
+    // Search
+    'search'                            => 'Vyhledávání',
+    'search_no_results'                 => 'Nenalezeny žádné výsledky',
+    'search_results_for'                => 'Výsledky pro',
+
+    // Social images
+    'social_images'                     => 'Vygenerován jeden obrázek pro soc. sítě.|Vygenerováno :count obrázků pro soc. sítě.',
+    'social_images_queue'               => 'Generování jednoho obrázku pro soc. sítě na pozadí.|Generování :count obrázků pro soc. sítě na pozadí.',
+
+    // Widgets
+    'widget_assets_title'               => 'Assety bez alternativního textu v :container',
+    'widget_assets_edit'                => 'Editovat tento Asset',
+    'widget_assets_explanation'         => 'Je důležité přidat alternativní text popisující vaše obrázky. Pomůže to uživatelům, kteří jsou závislí na asistenčních technologiích.',
+    'widget_assets_count'               => '{0}|{1}Máte :amount obrázek, který potřebuje vaši pozornost.|[2,4]Máte :amount obrázky, které potřebují vaši pozornost.|[5,*]Máte :amount obrázeků, které potřebují vaši pozornost.',
+    'widget_assets_done'                => 'Všechny Assety mají alternativní text.',
+];
