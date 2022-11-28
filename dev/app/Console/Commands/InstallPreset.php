14f57029e62d42ad81f102cb63921d65ef77d333
Rob de Kort <rob@studio1902.nl>
2022-11-28 22:35:17 +0100

    Start fresh with install command

--- dev/app/Console/Commands/InstallPreset.php
+++ dev/app/Console/Commands/InstallPreset.php
@@ -3,11 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\File;
 use Statamic\Console\RunsInPlease;
-use Statamic\Support\Arr;
-use Stringy\StaticStringy as Stringy;
-use Symfony\Component\Yaml\Yaml;
 
 class InstallPreset extends Command
 {
@@ -15,10 +11,8 @@ class InstallPreset extends Command
 
     protected $name = 'statamic:peak:install-preset';
     protected $description = "Install premade collections and page builder blocks into your site.";
-    protected $preset_name = '';
     protected $choices = '';
-    protected $filename = '';
-    protected $instructions = '';
+    protected $presets = [];
 
     public function handle()
     {
@@ -40,75 +34,5 @@ class InstallPreset extends Command
         // Copy news-fieldset.yaml.stub -> resources/fieldsets/news.yaml
         // Copy index_content.yaml.stub -> resources/fieldsets/index_content.yaml
         // Add `index_content` and `news` to page builder.
-
-        foreach($this->choices as $choice) {
-            $this->preset_name = Stringy::split($choice, ':')[0];
-            $this->filename = Stringy::between($choice, '[', ']');
-            $this->instructions = Stringy::between($choice, ': ', ' [');
-
-            try {
-                $this->checkExistence('Fieldset', "resources/fieldsets/{$this->filename}.yaml");
-                $this->checkExistence('Partial', "resources/views/page_builder/_{$this->filename}.antlers.html");
-
-                $this->copyStubs();
-                $this->updatePageBuilder();
-            } catch (\Exception $e) {
-                return $this->error($e->getMessage());
-            }
-
-            $this->info("Peak preset '{$this->preset_name}' installed.");
-        }
-    }
-
-    /**
-     * Check if a file doesn't already exist.
-     *
-     * @return bool|null
-     */
-    protected function checkExistence($type, $path)
-    {
-        if (File::exists(base_path($path))) {
-            throw new \Exception("{$type} '{$path}' already exists.");
-        }
-    }
-
-    /**
-     * Copy yaml and html stubs.
-     *
-     * @return bool|null
-     */
-    protected function copyStubs()
-    {
-        File::put(base_path("resources/fieldsets/{$this->filename}.yaml"), File::get(__DIR__."/stubs/blocks/{$this->filename}.yaml.stub"));
-        File::put(base_path("resources/views/page_builder/_{$this->filename}.antlers.html"), File::get(__DIR__."/stubs/blocks/{$this->filename}.antlers.html.stub"));
-    }
-
-    /**
-     * Update page_builder.yaml.
-     *
-     * @return bool|null
-     */
-    protected function updatePageBuilder()
-    {
-        $fieldset = Yaml::parseFile(base_path('resources/fieldsets/page_builder.yaml'));
-        $newSet = [
-            'display' => $this->preset_name,
-            'instructions' => $this->instructions,
-            'fields' => [
-                [
-                    'import' => $this->filename
-                ]
-            ]
-        ];
-
-        $existingSets = Arr::get($fieldset, 'fields.0.field.sets');
-        $existingSets[$this->filename] = $newSet;
-        $existingSets = collect($existingSets)->sortBy(function ($value, $key) {
-            return $key;
-        })->all();
-
-        Arr::set($fieldset, 'fields.0.field.sets', $existingSets);
-
-        File::put(base_path('resources/fieldsets/page_builder.yaml'), Yaml::dump($fieldset, 99, 2));
     }
 }
