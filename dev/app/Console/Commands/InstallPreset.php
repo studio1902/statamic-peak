b33a99300c5e7320ded65b15b8f8e13c6b410cf9
Rob de Kort <rob@studio1902.nl>
2022-11-29 10:35:18 +0100

    Update news file array and file names

--- dev/app/Console/Commands/InstallPreset.php
+++ dev/app/Console/Commands/InstallPreset.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Statamic\Console\RunsInPlease;
