0a46bd89a265ae527675a88676bf3d7a6ceb2d8b
Rob de Kort <rob@studio1902.nl>
2022-11-30 10:13:58 +0100

    Add FAQ preset

--- dev/app/Console/Commands/InstallPresetPresets.php
+++ dev/app/Console/Commands/InstallPresetPresets.php
@@ -89,6 +89,55 @@ trait InstallPresetPresets {
                 ]
             ],
             [
+                'handle' => 'faq',
+                'name' => 'FAQ',
+                'description' => 'A FAQ collection with a page builder set (including JSON-ld).',
+                'operations' => [
+                    [
+                        'type' => 'copy',
+                        'input' => 'faq_blueprint.yaml.stub',
+                        'output' => 'resources/blueprints/collections/faq/faq.yaml'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'faq_collection.yaml.stub',
+                        'output' => 'content/collections/faq.yaml'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'faq_fieldset.yaml.stub',
+                        'output' => 'resources/fieldsets/faq.yaml'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'faq.antlers.html.stub',
+                        'output' => 'resources/views/page_builder/_faq.antlers.html'
+                    ],
+                    [
+                        'type' => 'copy',
+                        'input' => 'question.antlers.html.stub',
+                        'output' => 'resources/views/components/_question.antlers.html'
+                    ],
+                    [
+                        'type' => 'update_page_builder',
+                        'block' => [
+                            'name' => 'FAQ',
+                            'instructions' => 'List frequently asked questions in an accordion.',
+                            'handle' => 'faq',
+                        ]
+                    ],
+                    [
+                        'type' => 'update_role',
+                        'role' => 'editor',
+                        'permissions' => ['view faq entries', 'edit faq entries', 'create faq entries', 'delete faq entries', 'publish faq entries', 'reorder faq entries', 'edit other authors faq entries', 'publish other authors faq entries', 'delete other authors faq entries']
+                    ],
+                    [
+                        'type' => 'notify',
+                        'content' => "\nAdd this to your `config/statamic/cp.php` widgets array:\n\n[\n\t'type' => 'collection',\n\t'collection' => 'faq',\n\t'width' => 50\n],\n"
+                    ]
+                ],
+            ],
+            [
                 'handle' => 'news',
                 'name' => 'News',
                 'description' => 'A dated news collection with index and show templates (including JSON-ld) and a page builder set.',
