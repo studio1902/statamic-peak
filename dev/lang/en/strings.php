36bf5fd9fd6ce828dd5a2c6d0c672fbf474aa92f
Rob de Kort <rob@studio1902.nl>
2022-12-03 14:26:53 +0100

    Really fix grammar on that pesky string

--- dev/lang/en/strings.php
+++ dev/lang/en/strings.php
@@ -97,6 +97,6 @@ return [
     'widget_assets_title'               => 'Assets without Alt Text in :container',
     'widget_assets_edit'                => 'Edit this Asset',
     'widget_assets_explanation'         => 'It\'s important to add alt text describing your images. This helps users who depend on assistive technology.',
-    'widget_assets_count'               => '{0}You have :amount image that need attention.|{1}You have :amount image that needs attention.|[2,*]You have at least :amount images that need attention.',
+    'widget_assets_count'               => '{0}You have :amount images that need attention.|{1}You have :amount image that needs attention.|[2,*]You have at least :amount images that need attention.',
     'widget_assets_done'                => 'All assets have an alt text.',
 ];
