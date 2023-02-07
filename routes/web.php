91a4b8ed02b9e713ef6e0e4c60e34829ed435471
Marco Rieser <marco@rieser.dev>
2023-02-07 16:18:09 +0100

    Rely on `statamic-peak-seo` addon (#298)
    * Remove all unused views
    
    * Remove unused jobs, actions and widgets
    
    * Remove unused fieldsets
    
    * Remove unused routes
    
    * Reference fieldsets from `statamic-peak-seo` addon in blueprints
    
    * Reflect changes in `starter-kit.yaml`

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\DynamicToken;
 use Illuminate\Support\Facades\Route;
-use Statamic\Facades\Site;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,33 +17,8 @@ use Statamic\Facades\Site;
 // Dynamic Token route for posting a form with Ajax.
 Route::get('/!/DynamicToken/refresh', [DynamicToken::class, 'getRefresh']);
 
-// The Sitemap Index route for listing sitemaps of all (multi)sites.
-Route::statamic('/sitemaps.xml', 'sitemap/sitemaps', [
-    'layout' => null,
-    'content_type' => 'application/xml'
-]);
-
-// The Default Site Sitemap route.
-Route::statamic('/sitemap.xml', 'sitemap/sitemap', [
-    'layout' => null,
-    'content_type' => 'application/xml'
-]);
-
-// The Multisite Site Sitemap route(s).
-Route::statamic('/{site_handle}/sitemap.xml', 'sitemap/sitemap', [
-    'layout' => null,
-    'content_type' => 'application/xml'
-]);
-
 // The Manifest route to the manifest.json
 Route::statamic('/site.webmanifest', 'manifest/manifest', [
     'layout' => null,
     'content_type' => 'application/json'
 ]);
-
-// The Social Image route to generate social images.
-// Site::all()->each(function ($site) {
-//     Route::statamic("{$site->url()}/social-images/{id}", 'social_images', [
-//         'layout' => null,
-//     ]);
-// });
