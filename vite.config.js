fd5bfcd33e67f39932a147cc0e54d09109d1c7ec
Marco Rieser <marco@rieser.dev>
2023-03-17 13:12:13 +0100

    Remove unnecessary semicolons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import laravel from 'laravel-vite-plugin'
 import { defineConfig, loadEnv } from 'vite'
-import { networkInterfaces } from 'os';
-import VitePluginBrowserSync from 'vite-plugin-browser-sync';
+import { networkInterfaces } from 'os'
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ command, mode }) => {
@@ -25,7 +25,7 @@ export default defineConfig(({ command, mode }) => {
                             proxy: env.APP_URL,
                             notify: false,
                         },
-                    });
+                    })
                 }
             })(),
         ],
