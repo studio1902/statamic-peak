27eced855465f1a130b5b570dee1f3bfb8e988ab
Marco Rieser <marco@rieser.dev>
2023-04-03 14:56:05 +0200

    Add support for secured sites and browsersync to vite (#307)
    * Add type hints for vite
    
    * Use valet cetificates in vite if the site is secured
    
    * Install `vite-plugin-browser-sync` npm package
    
    * Add npm script for vite with browsersync env variable
    
    * Ensure vite uses current ip as host
    
    * Add the browsersync plugin to vite if the env variable is present
    
    * Remove unnecessary semicolons
    
    * optimize ternary operator
    
    * ensure browsersync is only used if device is connected to a network
    
    * add whitespace after assignment operator
    
    * open external url with browsersync

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,38 @@
 import laravel from 'laravel-vite-plugin'
 import { defineConfig, loadEnv } from 'vite'
+import { networkInterfaces } from 'os'
+import VitePluginBrowserSync from 'vite-plugin-browser-sync'
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
+    const ip = Object.values(networkInterfaces()).flat().find((i) => i.family == 'IPv4' && !i.internal)?.address
+    const useBrowsersync = env.VITE_BROWSERSYNC && ip
+
     return {
         plugins: [
             laravel({
                 refresh: true,
+                valetTls: env.APP_URL.startsWith('https://') ? env.APP_URL.replace(/^https?:\/\//, '') : false,
                 input: [
                     'resources/css/site.css',
                     'resources/js/site.js',
                 ]
-            })
+            }),
+            (() => {
+                if (useBrowsersync) {
+                    return VitePluginBrowserSync({
+                        bs: {
+                            proxy: env.APP_URL,
+                            notify: false,
+                            open: 'external',
+                        },
+                    })
+                }
+            })(),
         ],
         server: {
+            host: useBrowsersync ? ip : false,
             open: env.APP_URL
         }
     }
