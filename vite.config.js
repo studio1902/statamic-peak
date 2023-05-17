6cffeacc9555d8446a092d1fb5706dc979a4d296
Marco Rieser <marco@rieser.dev>
2023-05-17 11:16:58 +0200

    Disable BrowserSync option for secured sites

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,46 @@
 import laravel from 'laravel-vite-plugin'
-import { defineConfig, loadEnv } from 'vite'
-import { networkInterfaces } from 'os'
+import {defineConfig, loadEnv} from 'vite'
+import {networkInterfaces} from 'os'
 import VitePluginBrowserSync from 'vite-plugin-browser-sync'
 
 /** @type {import('vite').UserConfig} */
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({command, mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
     const ip = Object.values(networkInterfaces()).flat().find((i) => i.family == 'IPv4' && !i.internal)?.address
-    const useBrowsersync = env.VITE_BROWSERSYNC && ip
+    const isSecuredSite = env.APP_URL.startsWith('https://')
+    const url = env.APP_URL
+    const host = url.replace(/^https?:\/\//, '')
+    const useBrowserSync = env.VITE_BROWSERSYNC && ip
+
+    if (useBrowserSync && isSecuredSite) {
+        console.log('\x1b[31m%s\x1b[0m', 'Sorry, we didn\'t manage to get BrowserSync working with secured sites.');
+        console.log('\x1b[31m%s\x1b[0m', 'If you can provide a solution, please enlighten us (https://github.com/studio1902/statamic-peak/pulls).');
+        process.exit(1)
+    }
 
     return {
         plugins: [
             laravel({
                 refresh: true,
-                valetTls: env.APP_URL.startsWith('https://') ? env.APP_URL.replace(/^https?:\/\//, '') : false,
+                valetTls: isSecuredSite ? host : false,
                 input: [
                     'resources/css/site.css',
                     'resources/js/site.js',
                 ]
             }),
             (() => {
-                if (useBrowsersync) {
-                    return VitePluginBrowserSync({
-                        bs: {
-                            proxy: env.APP_URL,
-                            notify: false,
-                            open: 'external',
-                        },
-                    })
-                }
-            })(),
+                return useBrowserSync ? VitePluginBrowserSync({
+                    bs: {
+                        proxy: url,
+                        notify: false,
+                        open: 'external',
+                    },
+                }) : null;
+            })()
         ],
         server: {
-            host: useBrowsersync ? ip : false,
-            open: env.APP_URL
+            host: useBrowserSync ? ip : host,
+            open: useBrowserSync ? false : url,
         }
     }
 });
