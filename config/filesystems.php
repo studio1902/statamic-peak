12b9e19e1299982cd5a09ee14b1bf2cc087f2e25
Håvard Grimelid <havard@grimelid.com>
2023-05-22 09:57:47 +0200

    Update link to docs about container visibility

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            // 'visibility' => 'public', // https://statamic.dev/assets#visibility
+            // 'visibility' => 'public', // https://statamic.dev/assets#container-visibility
         ],
 
         'images' => [
