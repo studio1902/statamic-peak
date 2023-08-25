40403a090a7289358a772663c2f7884f2f543234
Håvard <havard@grimelid.com>
2023-08-25 11:01:50 +0200

    Renamed Norwegian Bokmål locale from no to nb and update translations (#341)
    * Renamed Norwegian Bokmål locale from `no` to `nb`
    
    * Update Norwegian Bokmål translation

---
+++ lang/nb/strings.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+    // Contact form
+    'form_error' => 'Det er feil med skjemaet ditt. Reparer dem og prøv igjen.',
+    'form_honeypot' => 'Faks',
+    'form_mail_body_owner' => 'Kontaktskjema er sendt.',
+    'form_mail_body_sender' => 'Takk for melding. Du hører fra oss snart.',
+    'form_mail_closing' => 'Vennlig hilsen',
+    'form_mail_from' => config('app.name'),
+    'form_mail_greeting' => 'Kjære',
+    'form_mail_logo' => 'https://cdn.studio1902.nl/assets/statamic-peak/statamic-peak-logo.png',
+    'form_mail_submitted' => 'Levert',
+    'form_mail_title' => 'Kontaktskjema',
+    'form_mail_url' => config('app.url'),
+    'form_send' => 'Send melding',
+    'form_subject_received' => 'Skjema mottatt',
+    'form_subject_sent' => 'Skjema sendt',
+    'form_success' => 'Tusen takk, vi har mottatt meldingen din og kontakter deg så snart som mulig.',
+
+    // Cookie banner
+    'cookie_accept_all' => 'Aksepter alle',
+    'cookie_accept_selected' => 'Aksepter valgte',
+    'cookie_ads' => 'Reklame og sporing',
+    'cookie_ads_explanation' => 'Tredjepartsinformasjonskapsler, trengs ikke.',
+    'cookie_analytics' => 'Analyser',
+    'cookie_analytics_explanation' => 'Tredjepartsinformasjonskapsler, trengs ikke.',
+    'cookie_explanation' => 'Denne siden bruker informasjonskapsler som trenger godkjenning.',
+    'cookie_embeds' => 'Innebygd video',
+    'cookie_embeds_explanation' => 'Tredjeparts informasjonskapsler som brukes til innebygde videoer som muligens kan spore deg.',
+    'cookie_embeds_disabled' => 'Godta informasjonskapsler for innebygd video for å se denne videoen.',
+    'cookie_functional' => 'Fungerende',
+    'cookie_functional_explanation' => 'Førstepartsinformasjonskapsler som trengs for at nettstedet skal fungere. Disse kan ikke slås av.',
+    'cookie_title' => 'Informasjonskapsler',
+    'cookie_learn_more' => 'Les mer',
+    'cookie_change_preferences' => 'Endre innstillinger for informasjonskapsler',
+
+    // Footer
+    'on' => 'på',
+
+    // Navigation
+    'close' => 'Lukk',
+    'main_navigation' => 'Hovedmeny',
+    'menu' => 'Meny',
+    'nav_close' => 'Lukk meny',
+    'nav_open' => 'Åpne meny',
+    'subnav_close' => 'Lukk undermeny',
+    'subnav_open' => 'Åpne undermeny',
+    'skip_to_content' => 'Gå direkte til innhold',
+
+    // Other
+    'no_results' => 'Det er ingen innlegg for øyeblikket.',
+    'no_script' => 'Dette nettstedet trenger Javascript for at alle deler skal fungere korrekt. Opplevelsen kan variere om du ikke bruker Javascript.',
+
+    // Pagination
+    'next' => 'neste',
+    'of' => 'av',
+    'previous' => 'forrige',
+
+    // Social images
+    'social_images' => 'Genererer et bilde for sosiale medium.|Genererer :count bilder for sosiale medium.',
+    'social_images_queue' => 'Genererer et bilde for sosiale medium bakgrunnen.|Genererer :count bilder for sosiale medium i bakgrunnen.',
+
+    // Widgets
+    'widget_assets_title' => 'Mediaelementer uten alternativ tekst i :container',
+    'widget_assets_edit' => 'Rediger dette elementet',
+    'widget_assets_explanation' => 'Det er viktig å legge til alternativ tekst som beskriver bildene dine. Dette hjelper brukere som er avhengige av hjelpemidler.',
+    'widget_assets_count' => '{0}Du har :amount bilder som trenger oppmerksomhet.|{1}Du har :amount bilde som trenger oppmerksomhet.|[2,*]Du har minst :amount bilder som trenger oppmerksomhet.',
+    'widget_assets_done' => 'Alle mediaelementer har alternativ tekst.',
+];
