ccd9da2bf1f20608fac3f54cbe236d6796a62cda
Marco Rieser <marco@rieser.dev>
2023-09-13 08:35:23 +0200

    rearrange methods

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -46,86 +46,6 @@ class StarterKitPostInstall
         $this->writeEnvAndReadme();
     }
 
-    protected function loadPresetEnvAndReadme(): void
-    {
-        $this->env = app('files')->get(base_path('.env.example'));
-        $this->readme = app('files')->get(base_path('README.md'));
-    }
-
-    protected function setAppName(): void
-    {
-        $appName = text(
-            label: 'What should be your app name?',
-            placeholder: 'Statamic Peak',
-            required: true,
-        );
-
-        $appName = preg_replace('/([\'|\"|#])/m', '', $appName);
-
-        $this->replaceInEnv('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"");
-        $this->replaceInReadme('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"");
-    }
-
-    protected function replaceInEnv(string $search, string $replace): void
-    {
-        $this->env = str_replace($search, $replace, $this->env);
-    }
-
-    protected function replaceInReadme(string $search, string $replace): void
-    {
-        $this->readme = str_replace($search, $replace, $this->readme);
-    }
-
-    protected function setAppUrl(): void
-    {
-        $appUrl = env('APP_URL');
-
-        $this->replaceInEnv('APP_URL=', "APP_URL=\"{$appUrl}\"");
-    }
-
-    protected function setAppKey(): void
-    {
-        $appKey = env('APP_KEY');
-
-        $this->replaceInEnv('APP_KEY=', "APP_KEY=\"{$appKey}\"");
-        $this->replaceInReadme('APP_KEY=', "APP_KEY=\"{$appKey}\"");
-    }
-
-    protected function useDebugbar(): void
-    {
-        if (confirm(label: 'Do you want to use the debugbar?', default: false,)) {
-            return;
-        }
-
-        $this->replaceInEnv('DEBUGBAR_ENABLED=true', 'DEBUGBAR_ENABLED=false');
-    }
-
-    protected function useImagick(): void
-    {
-        if (!confirm(label: 'Do you want use Imagick as an image processor instead of GD?', default: true,)) {
-            return;
-        }
-
-        $this->replaceInEnv('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick');
-        $this->replaceInReadme('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick');
-    }
-
-    protected function enableSaveCachedImages(): void
-    {
-        if (!confirm(label: 'Do you want to enable `SAVE_CACHED_IMAGES` (slower initial page load)?', default: false,)) {
-            return;
-        }
-
-        $this->replaceInEnv('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true');
-        $this->replaceInReadme('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true');
-    }
-
-    protected function writeEnvAndReadme(): void
-    {
-        app('files')->put(base_path('.env'), $this->env);
-        app('files')->put(base_path('README.md'), $this->readme);
-    }
-
     protected function initializeGitAndConfigureGitignore(): void
     {
         if (!confirm(label: 'Do you want to init a git repo and configure gitignore?', default: true,)) {
@@ -137,54 +57,6 @@ class StarterKitPostInstall
         $this->excludeBuildFolderFromGit();
         $this->excludeUsersFolderFromGit();
         $this->excludeFormsFolderFromGit();
-    }
-
-    protected function initializeGitRepo(): void
-    {
-        $this->runCommand('git init', fn() => info('Repo initialised.'));
-    }
-
-    protected function runCommand(string $command, callable $callback): void
-    {
-        $process = new Process(explode(' ', $command));
-        try {
-            $process->mustRun();
-            $callback();
-        } catch (ProcessFailedException $exception) {
-            error($exception->getMessage());
-        }
-    }
-
-    protected function excludeBuildFolderFromGit(): void
-    {
-        if (!confirm(label: 'Do you want to exclude the `public/build` folder from git?', default: true,)) {
-            return;
-        }
-
-        $this->appendToGitignore('/public/build/');
-    }
-
-    protected function appendToGitignore(string $toIgnore): void
-    {
-        app('files')->append(base_path('.gitignore'), "\n{$toIgnore}");
-    }
-
-    protected function excludeUsersFolderFromGit(): void
-    {
-        if (!confirm(label: 'Do you want to exclude the `users` folder from git?', default: false,)) {
-            return;
-        }
-
-        $this->appendToGitignore('/users');
-    }
-
-    protected function excludeFormsFolderFromGit(): void
-    {
-        if (!confirm(label: 'Do you want to exclude the `storage/form` folder from git?', default: false,)) {
-            return;
-        }
-
-        $this->appendToGitignore('/storage/forms');
     }
 
     protected function installNodeDependencies(): void
@@ -204,16 +76,6 @@ class StarterKitPostInstall
 
         $this->installPuppeteer();
         $this->installBrowsershot();
-    }
-
-    protected function installPuppeteer(): void
-    {
-        $this->runCommand('npm i puppeteer', fn() => info('Puppeteer installed.'));
-    }
-
-    protected function installBrowsershot(): void
-    {
-        $this->runCommand('composer require spatie/browsershot', fn() => info('Browsershot installed.'));
     }
 
     protected function installTranslations(): void
@@ -280,5 +142,143 @@ class StarterKitPostInstall
         warning('Run `php please peak:clear-site` to get rid of default content.');
         warning('Run `php please peak:install-preset` to install premade sets onto your website.');
         warning('Run `php please peak:install-block` to install premade blocks onto your page builder.');
+    }
+
+    protected function loadPresetEnvAndReadme(): void
+    {
+        $this->env = app('files')->get(base_path('.env.example'));
+        $this->readme = app('files')->get(base_path('README.md'));
+    }
+
+    protected function setAppName(): void
+    {
+        $appName = text(
+            label: 'What should be your app name?',
+            placeholder: 'Statamic Peak',
+            required: true,
+        );
+
+        $appName = preg_replace('/([\'|\"|#])/m', '', $appName);
+
+        $this->replaceInEnv('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"");
+        $this->replaceInReadme('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"");
+    }
+
+    protected function setAppUrl(): void
+    {
+        $appUrl = env('APP_URL');
+
+        $this->replaceInEnv('APP_URL=', "APP_URL=\"{$appUrl}\"");
+    }
+
+    protected function setAppKey(): void
+    {
+        $appKey = env('APP_KEY');
+
+        $this->replaceInEnv('APP_KEY=', "APP_KEY=\"{$appKey}\"");
+        $this->replaceInReadme('APP_KEY=', "APP_KEY=\"{$appKey}\"");
+    }
+
+    protected function useDebugbar(): void
+    {
+        if (confirm(label: 'Do you want to use the debugbar?', default: false,)) {
+            return;
+        }
+
+        $this->replaceInEnv('DEBUGBAR_ENABLED=true', 'DEBUGBAR_ENABLED=false');
+    }
+
+    protected function useImagick(): void
+    {
+        if (!confirm(label: 'Do you want use Imagick as an image processor instead of GD?', default: true,)) {
+            return;
+        }
+
+        $this->replaceInEnv('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick');
+        $this->replaceInReadme('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick');
+    }
+
+    protected function enableSaveCachedImages(): void
+    {
+        if (!confirm(label: 'Do you want to enable `SAVE_CACHED_IMAGES` (slower initial page load)?', default: false,)) {
+            return;
+        }
+
+        $this->replaceInEnv('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true');
+        $this->replaceInReadme('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true');
+    }
+
+    protected function writeEnvAndReadme(): void
+    {
+        app('files')->put(base_path('.env'), $this->env);
+        app('files')->put(base_path('README.md'), $this->readme);
+    }
+
+    protected function initializeGitRepo(): void
+    {
+        $this->runCommand('git init', fn() => info('Repo initialised.'));
+    }
+
+    protected function excludeBuildFolderFromGit(): void
+    {
+        if (!confirm(label: 'Do you want to exclude the `public/build` folder from git?', default: true,)) {
+            return;
+        }
+
+        $this->appendToGitignore('/public/build/');
+    }
+
+    protected function excludeUsersFolderFromGit(): void
+    {
+        if (!confirm(label: 'Do you want to exclude the `users` folder from git?', default: false,)) {
+            return;
+        }
+
+        $this->appendToGitignore('/users');
+    }
+
+    protected function excludeFormsFolderFromGit(): void
+    {
+        if (!confirm(label: 'Do you want to exclude the `storage/form` folder from git?', default: false,)) {
+            return;
+        }
+
+        $this->appendToGitignore('/storage/forms');
+    }
+
+    protected function runCommand(string $command, callable $callback): void
+    {
+        $process = new Process(explode(' ', $command));
+        try {
+            $process->mustRun();
+            $callback();
+        } catch (ProcessFailedException $exception) {
+            error($exception->getMessage());
+        }
+    }
+
+    protected function installPuppeteer(): void
+    {
+        $this->runCommand('npm i puppeteer', fn() => info('Puppeteer installed.'));
+    }
+
+    protected function installBrowsershot(): void
+    {
+        $this->runCommand('composer require spatie/browsershot', fn() => info('Browsershot installed.'));
+    }
+
+    protected function replaceInEnv(string $search, string $replace): void
+    {
+        $this->env = str_replace($search, $replace, $this->env);
+    }
+
+    protected function replaceInReadme(string $search, string $replace): void
+    {
+        $this->readme = str_replace($search, $replace, $this->readme);
+    }
+
+    protected function appendToGitignore(string $toIgnore): void
+    {
+        app('files')->append(base_path('.gitignore'), "\n{$toIgnore}");
     }
 }
