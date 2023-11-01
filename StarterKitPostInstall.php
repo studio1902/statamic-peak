56c4350895611fa49c6d00c5f9f7931ff5635819
Marco Rieser <marco@rieser.dev>
2023-11-01 15:48:39 +0100

    Migrate PostInstall Laravel Prompts (#348)
    * migrate post install script to laravel prompts
    
    * combine `str_replace` calls
    
    * require laravel prompts
    
    * require laravel 10 instead of just the prompts
    
    * extract stuff to its own methods
    
    * remove default for app name but require it
    
    * rearrange methods
    
    * add spinner for processes
    
    * add default for app name (temporary fix)
    
    * add success message for after overwriting the .env file
    
    * restore interactivity for prompts
    
    * remove trailing commas
    
    * set sensible defaults
    
    * use new LaravelLang stuff
    
    * reimplement previous laravel lang installation

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -1,139 +1,345 @@
 <?php
 
+use Illuminate\Support\Collection;
+use Laravel\Prompts\Prompt;
+use LaravelLang\Locales\Facades\Locales;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\spin;
+use function Laravel\Prompts\text;
+use function Laravel\Prompts\warning;
+
 
 class StarterKitPostInstall
 {
-    public function handle($console)
+    protected string $env = '';
+    protected string $readme = '';
+    protected bool $interactive = true;
+
+    public function handle($console): void
     {
-        if ($console->confirm('Do you want overwrite your `.env` file with the Peak presets?', true)) {
-            $appName = $console->ask('What should be your app name?');
-            $appName = preg_replace('/([\'|\"|#])/m', '', $appName);
-            $debugBarEnabled = $console->confirm('Do you want to use the debugbar?', false);
-            $originalAppUrl = env('APP_URL');
-            $originalAppKey = env('APP_KEY');
+        $this->applyInteractivity($console);
+        $this->overwriteEnvWithPresets();
+        $this->initializeGitAndConfigureGitignore();
+        $this->installNodeDependencies();
+        $this->installPuppeteerAndBrowsershot();
+        $this->installTranslations();
+        $this->starPeakRepo();
+        $this->finish();
+    }
 
-            $env = app('files')->get(base_path('.env.example'));
-            $env = str_replace('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"", $env);
-            $env = str_replace('APP_URL=', "APP_URL=\"{$originalAppUrl}\"", $env);
-            $env = str_replace('APP_KEY=', "APP_KEY=\"{$originalAppKey}\"", $env);
-            if (! $debugBarEnabled) {
-                $env = str_replace('DEBUGBAR_ENABLED=true', 'DEBUGBAR_ENABLED=false', $env);
-            }
+    protected function applyInteractivity($console): void
+    {
+        $this->interactive = !$console->option('no-interaction');
 
-            $readme = app('files')->get(base_path('README.md'));
-            $readme = str_replace('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"", $readme);
-            $readme = str_replace('APP_KEY=', "APP_KEY=\"{$originalAppKey}\"", $readme);
+        /**
+         * Interactivity should be inherited but seems like there is a bug in Prompts where it stays
+         * without interaction when a command was run before with `--no-interaction` flag.
+         */
+        Prompt::interactive($this->interactive);
+    }
 
-            if ($console->confirm('Do you want use Imagick as an image processor instead of GD?', true)) {
-                $env = str_replace('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick', $env);
-                $readme = str_replace('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick', $readme);
-            }
-
-            if ($console->confirm('Do you want to enable `SAVE_CACHED_IMAGES` (slower initial page load)?', false)) {
-                $env = str_replace('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true', $env);
-                $readme = str_replace('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true', $readme);
-            }
-
-            app('files')->put(base_path('.env'), $env);
-            app('files')->put(base_path('README.md'), $readme);
+    protected function overwriteEnvWithPresets(): void
+    {
+        if (!confirm(label: 'Do you want overwrite your `.env` file with the Peak presets?', default: true)) {
+            return;
         }
 
-        if ($console->confirm('Do you want to init a git repo and configure gitignore?', true)) {
-            $process = new Process(['git', 'init']);
-            try {
-                $process->mustRun();
-                $console->info('Repo initialised.');
-            } catch (ProcessFailedException $exception) {
-                $console->info($exception->getMessage());
-            }
+        $this->loadPresetEnvAndReadme();
 
-            if ($console->confirm('Do you want to exclude the `public/build` folder from git?', true)) {
-                app('files')->append(base_path('.gitignore'), "\n/public/build/");
-            }
+        $this->setAppName();
+        $this->setAppUrl();
+        $this->setAppKey();
 
-            if ($console->confirm('Do you want to exclude the `users` folder from git?', false)) {
-                app('files')->append(base_path('.gitignore'), "\n/users");
-            }
+        $this->useDebugbar();
+        $this->useImagick();
+        $this->enableSaveCachedImages();
 
-            if ($console->confirm('Do you want to exclude the `storage/form` folder from git?', false)) {
-                app('files')->append(base_path('.gitignore'), "\n/storage/forms");
-            }
+        $this->writeEnvAndReadme();
+
+        info("[✓] `.env` file overwritten.");
+    }
+
+    protected function initializeGitAndConfigureGitignore(): void
+    {
+        if (!confirm(label: 'Do you want to init a git repo and configure gitignore?', default: true)) {
+            return;
         }
 
-        if ($console->confirm('Do you want to install npm dependencies?', true)) {
-            $process = new Process(['npm', 'i']);
-            try {
-                $process->mustRun();
-                $console->info('Dependencies installed.');
-            } catch (ProcessFailedException $exception) {
-                $console->info($exception->getMessage());
-            }
+        $this->initializeGitRepo();
+
+        $this->excludeBuildFolderFromGit();
+        $this->excludeUsersFolderFromGit();
+        $this->excludeFormsFolderFromGit();
+    }
+
+    protected function installNodeDependencies(): void
+    {
+        if (!confirm(label: 'Do you want to install npm dependencies?', default: true)) {
+            return;
         }
 
-        if ($console->confirm('Do you want to `npm i puppeteer` and `composer require spatie/browsershot` for generating social images?', true)) {
-            $process = new Process(['npm', 'i', 'puppeteer']);
-            try {
-                $process->mustRun();
-                $console->info('Puppeteer installed.');
-            } catch (ProcessFailedException $exception) {
-                $console->info($exception->getMessage());
-            }
+        $this->run(
+            command: 'npm i',
+            successMessage: 'npm dependencies installed.',
+            processingMessage: 'Installing npm dependencies...',
+        );
+    }
 
-            $process = new Process(['composer', 'require', 'spatie/browsershot']);
-            try {
-                $process->mustRun();
-                $console->info('Browsershot installed.');
-            } catch (ProcessFailedException $exception) {
-                $console->info($exception->getMessage());
-            }
+    protected function installPuppeteerAndBrowsershot(): void
+    {
+        if (!confirm(label: 'Do you want to `npm i puppeteer` and `composer require spatie/browsershot` for generating social images?', default: true)) {
+            return;
         }
 
-        if ($console->confirm('Do you want to install missing Laravel translation files using the Laravel Lang package?', true)) {
-            $process = new Process(['composer', 'require', 'laravel-lang/common', '--dev']);
-            try {
-                $process->mustRun();
-                $console->info('Laravel Lang installed.');
-            } catch (ProcessFailedException $exception) {
-                $console->info($exception->getMessage());
-            }
+        $this->installPuppeteer();
+        $this->installBrowsershot();
+    }
 
-            $console->info('Enter the handles of the languages you want to install. Press enter when you\'re done.');
-            do {
-                if ($handle = $console->ask('Handle of language (e.g. "nl")')) {
-                    $process = new Process(['php', 'artisan', 'lang:add', $handle]);
-                    try {
-                        $process->mustRun();
-                        $console->info("Language \"{$handle}\" installed.");
-                    } catch (ProcessFailedException $exception) {
-                        $console->info($exception->getMessage());
-                    }
-                }
-            } while ($handle !== null);
+    protected function installTranslations(): void
+    {
+        if (!confirm(label: 'Do you want to install missing Laravel translation files using the Laravel Lang package?', default: $this->interactive)) {
+            return;
         }
 
-        if ($console->confirm('Would you like to star the Peak repo?', false)) {
-            if (PHP_OS_FAMILY == 'Darwin') {
-                exec('open https://github.com/studio1902/statamic-peak');
-            }
-            if (PHP_OS_FAMILY == 'Windows') {
-                exec('start https://github.com/studio1902/statamic-peak');
-            }
-            if (PHP_OS_FAMILY == 'Linux') {
-                exec('xdg-open https://github.com/studio1902/statamic-peak');
-            }
-
-            $console->info('Thank you!');
+        if (!$this->installLaravelLang()) {
+            error('Could not install Laravel Lang.');
+            return;
         }
 
-        $console->info('<info>[✓]</info> Peak is installed. Enjoy the view!');
-        $console->newline();
-        $console->warn('Run `php please peak:clear-site` to get rid of default content.');
-        $console->newline();
-        $console->warn('Run `php please peak:install-preset` to install premade sets onto your website.');
-        $console->newline();
-        $console->warn('Run `php please peak:install-block` to install premade blocks onto your page builder.');
-        $console->newline();
+        $this->selectLanguagesToInstall();
+    }
+
+    protected function starPeakRepo(): void
+    {
+        if (!confirm(label: 'Would you like to star the Peak repo?', default: false)) {
+            return;
+        }
+
+        if (PHP_OS_FAMILY === 'Darwin') {
+            exec('open https://github.com/studio1902/statamic-peak');
+        }
+
+        if (PHP_OS_FAMILY === 'Windows') {
+            exec('start https://github.com/studio1902/statamic-peak');
+        }
+
+        if (PHP_OS_FAMILY === 'Linux') {
+            exec('xdg-open https://github.com/studio1902/statamic-peak');
+        }
+
+        info('Thank you!');
+    }
+
+    protected function finish(): void
+    {
+        info('[✓] Peak is installed. Enjoy the view!');
+        warning('Run `php please peak:clear-site` to get rid of default content.');
+        warning('Run `php please peak:install-preset` to install premade sets onto your website.');
+        warning('Run `php please peak:install-block` to install premade blocks onto your page builder.');
+    }
+
+    protected function loadPresetEnvAndReadme(): void
+    {
+        $this->env = app('files')->get(base_path('.env.example'));
+        $this->readme = app('files')->get(base_path('README.md'));
+    }
+
+    protected function setAppName(): void
+    {
+        $appName = text(
+            label: 'What should be your app name?',
+            placeholder: 'Statamic Peak',
+            default: $this->interactive ? '' : 'Statamic Peak',
+            required: true,
+        );
+
+        $appName = preg_replace('/([\'|\"|#])/m', '', $appName);
+
+        $this->replaceInEnv('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"");
+        $this->replaceInReadme('APP_NAME="Statamic Peak"', "APP_NAME=\"{$appName}\"");
+    }
+
+    protected function setAppUrl(): void
+    {
+        $appUrl = env('APP_URL');
+
+        $this->replaceInEnv('APP_URL=', "APP_URL=\"{$appUrl}\"");
+    }
+
+    protected function setAppKey(): void
+    {
+        $appKey = env('APP_KEY');
+
+        $this->replaceInEnv('APP_KEY=', "APP_KEY=\"{$appKey}\"");
+        $this->replaceInReadme('APP_KEY=', "APP_KEY=\"{$appKey}\"");
+    }
+
+    protected function useDebugbar(): void
+    {
+        if (confirm(label: 'Do you want to use the debugbar?', default: false)) {
+            return;
+        }
+
+        $this->replaceInEnv('DEBUGBAR_ENABLED=true', 'DEBUGBAR_ENABLED=false');
+    }
+
+    protected function useImagick(): void
+    {
+        if (!confirm(label: 'Do you want use Imagick as an image processor instead of GD?', default: true)) {
+            return;
+        }
+
+        $this->replaceInEnv('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick');
+        $this->replaceInReadme('#IMAGE_MANIPULATION_DRIVER=imagick', 'IMAGE_MANIPULATION_DRIVER=imagick');
+    }
+
+    protected function enableSaveCachedImages(): void
+    {
+        if (!confirm(label: 'Do you want to enable `SAVE_CACHED_IMAGES` (slower initial page load)?', default: false)) {
+            return;
+        }
+
+        $this->replaceInEnv('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true');
+        $this->replaceInReadme('SAVE_CACHED_IMAGES=false', 'SAVE_CACHED_IMAGES=true');
+    }
+
+    protected function writeEnvAndReadme(): void
+    {
+        app('files')->put(base_path('.env'), $this->env);
+        app('files')->put(base_path('README.md'), $this->readme);
+    }
+
+    protected function initializeGitRepo(): void
+    {
+        $this->run(
+            command: 'git init',
+            successMessage: 'Repo initialised.',
+            processingMessage: 'Initialising repo...'
+        );
+    }
+
+    protected function excludeBuildFolderFromGit(): void
+    {
+        if (!confirm(label: 'Do you want to exclude the `public/build` folder from git?', default: true)) {
+            return;
+        }
+
+        $this->appendToGitignore('/public/build/');
+    }
+
+    protected function excludeUsersFolderFromGit(): void
+    {
+        if (!confirm(label: 'Do you want to exclude the `users` folder from git?', default: false)) {
+            return;
+        }
+
+        $this->appendToGitignore('/users');
+    }
+
+    protected function excludeFormsFolderFromGit(): void
+    {
+        if (!confirm(label: 'Do you want to exclude the `storage/form` folder from git?', default: false)) {
+            return;
+        }
+
+        $this->appendToGitignore('/storage/forms');
+    }
+
+    protected function run(string $command, string $successMessage, string $processingMessage, ?string $errorMessage = null): bool
+    {
+        $process = new Process(explode(' ', $command));
+        try {
+            spin(fn() => $process->mustRun(), $processingMessage);
+
+            info("[✓] {$successMessage}");
+
+            return true;
+        } catch (ProcessFailedException $exception) {
+            error($errorMessage ?? $exception->getMessage());
+
+            return false;
+        }
+    }
+
+    protected function installPuppeteer(): void
+    {
+        $this->run(
+            command: 'npm i puppeteer',
+            successMessage: 'Puppeteer installed.',
+            processingMessage: 'Installing Puppeteer...',
+        );
+    }
+
+    protected function installBrowsershot(): void
+    {
+        $this->run(
+            command: 'composer require spatie/browsershot',
+            successMessage: 'Browsershot installed.',
+            processingMessage: 'Installing Browsershot...',
+        );
+    }
+
+    protected function installLaravelLang(): bool
+    {
+        return $this->run(
+            command: 'composer require laravel-lang/common --dev',
+            successMessage: 'Laravel Lang installed.',
+            processingMessage: 'Installing Laravel Lang...',
+        );
+    }
+
+    protected function selectLanguagesToInstall(): void
+    {
+        info('Enter the handles of the languages you want to install. Leave empty and press enter when you\'re done.');
+
+        $installedLanguages = collect();
+
+        do {
+            if (($handle = $this->selectLanguageToInstall($installedLanguages)) && $this->installLanguage($handle)) {
+                $installedLanguages->push($handle);
+            }
+        } while ($handle);
+    }
+
+    protected function replaceInEnv(string $search, string $replace): void
+    {
+        $this->env = str_replace($search, $replace, $this->env);
+    }
+
+    protected function replaceInReadme(string $search, string $replace): void
+    {
+        $this->readme = str_replace($search, $replace, $this->readme);
+    }
+
+    protected function appendToGitignore(string $toIgnore): void
+    {
+        app('files')->append(base_path('.gitignore'), "\n{$toIgnore}");
+    }
+
+    protected function selectLanguageToInstall(Collection $installedLanguages): string
+    {
+        return text(
+            label: 'Handle of language (submit empty when you\'re done)',
+            placeholder: 'en',
+            validate: fn(string $value) => match (true) {
+                $value && $installedLanguages->contains($value) => "Language \"{$value}\" already installed.",
+                default => null,
+            },
+            hint: $installedLanguages->isNotEmpty() ? 'Installed: ' . $installedLanguages->join(', ', ' and ') : '',
+        );
+    }
+
+    protected function installLanguage(string $handle): bool
+    {
+        return $this->run(
+            "php artisan lang:add {$handle}",
+            "Language \"{$handle}\" installed.",
+            "Installing language \"{$handle}\"...",
+            "Installation of language \"{$handle}\" failed."
+        );
     }
 }
