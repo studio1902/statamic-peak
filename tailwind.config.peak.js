fb815578187a4e49a2391efb1a020339c1148ba7
Rob de Kort <rob@studio1902.nl>
2024-02-14 20:54:42 +0100

    Improve stacks

--- tailwind.config.peak.js
+++ tailwind.config.peak.js
@@ -107,31 +107,39 @@ module.exports = {
       }),
       // Stack utilities.
       matchUtilities(
-        {
-          stack: (value) => ({
-            '> *': {
-              '--stack-space': value,
-            },
-            '> *:not(.no-space-y, .no-space-b) + *:not(.no-space-y, .no-space-t)': {
-              marginBlockStart: 'var(--stack-space, 4rem)'
-            },
-          }),
-        },
-        { values: theme('spacing') }
-      ),
-      // Stack gap utilities.
-      matchUtilities(
-        {
-          'stack-space': (value) => ({
+      {
+        'stack': (value) => ({
+          '> *': {
             '--stack-space': value,
-          }),
+          },
+          '> *:not(.no-space-y, .no-space-b) + *:not(.no-space-y, .no-space-t)': {
+            'margin-block-start': `var(--stack-item-space, var(--stack-space, ${theme('spacing.16')}))`
+          },
+        }),
+        'stack-space': (value) => ({
+          '--stack-item-space': value,
+          '&:is([class*="stack-"][class*="stack-space-"] > *)': {
+            '--stack-item-space': value,
+          },
+        }),
         },
         { values: theme('spacing') }
       ),
       addUtilities({
-          '*:is(.stack-collapse) + *:is(.stack-collapse)': {
-              '--stack-space': 0,
+        '[class*="stack-"][class*="stack-space-"]': {
+          '& > *': {
+            '--stack-item-space': 'initial',
           },
+        },
+        '.stack-space-inherit': {
+          '--stack-item-space': 'initial',
+          '&:is([class*="stack-"][class*="stack-space-"] > *)': {
+            '--stack-item-space': 'initial',
+          },
+        },
+        '*:is(.stack-space-collapse) + *:is(.stack-space-collapse)': {
+          '--stack-item-space': 0,
+        },
       }),
       // Render screen names in the breakpoint display.
       addBase(Object.entries(theme('screens'))
