f9d97008a30e517c6946db0104646eaebd4cc089
Rob de Kort <rob@studio1902.nl>
2024-08-07 12:36:23 +0200

    Use Rollup chunking

---
+++ public/build/assets/site-CaZ90IYH.js
@@ -0,0 +1,1 @@
+import{m as o}from"./alpinejs-CIMwf5AW.js";import{m,a as t,b as a,c as e}from"./@alpinejs-TglsjoKA.js";import{p as l}from"./laravel-precognition-alpine-mDtVp8TL.js";import"./laravel-precognition-DdcDqtPE.js";import"./axios-ugFzd9eS.js";import"./lodash-es-BeZXkzNc.js";window.Alpine=o;o.plugin([m,t,a,e,l]);o.start();
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import { defineConfig, loadEnv } from 'vite'
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
     return {
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: (id) => {
+                        if (id.includes('node_modules')) return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        },
         plugins: [
             laravel({
                 refresh: true,
