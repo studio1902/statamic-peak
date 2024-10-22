74b82bb6e1dc972e28a815e99c48c8935a13197d
Rob de Kort <rob@studio1902.nl>
2024-10-22 19:45:09 +0200

    Update dependencies and recompile assets

---
+++ public/build/assets/site-DxJBDtNm.js
@@ -0,0 +1,1 @@
+import{m as o}from"./alpinejs-BGcqaHQq.js";import{m,a as t,b as a,c as e}from"./@alpinejs-TglsjoKA.js";import{p as l}from"./laravel-precognition-alpine-Db-MIXDH.js";import"./laravel-precognition-taiq34mt.js";import"./axios-CYRnxaT6.js";import"./lodash-es-BeZXkzNc.js";window.Alpine=o;o.plugin([m,t,a,e,l]);o.start();
