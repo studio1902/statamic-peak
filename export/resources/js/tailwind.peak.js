b702ef19eea61b3d8d2bab477fedce86ddc7aea8
Rob de Kort <rob@studio1902.nl>
2025-01-16 19:51:23 +0100

    Generate stack utilities in CSS

--- export/resources/js/tailwind.peak.js
+++ export/resources/js/tailwind.peak.js
@@ -2,28 +2,7 @@ import plugin from 'tailwindcss/plugin'
 
 export default {
     plugins: [
-        plugin(function ({ matchUtilities, addBase, theme }) {
-            matchUtilities(
-                {
-                    stack: (value) => ({
-                        '> *': {
-                            '--stack-space': value,
-                        },
-                        '> *:not(.no-space-y, .no-space-b) + *:not(.no-space-y, .no-space-t)': {
-                            'margin-block-start': `var(--stack-item-space, var(--stack-space, ${theme(
-                                'spacing.16'
-                            )}))`,
-                        },
-                    }),
-                    'stack-space': (value) => ({
-                        '--stack-item-space': value,
-                        '&:is([class*="stack-"][class*="stack-space-"] > *)': {
-                            '--stack-item-space': value,
-                        },
-                    }),
-                },
-                { values: theme('spacing') }
-            ),
+        plugin(function ({ addBase, theme }) {
             // Render screen names in the breakpoint display.
             addBase(Object.entries(theme('screens'))
                 .filter((value) => typeof value[1] == 'string')
