7042478fb198095acfcb7e566698f640d0312123
Rob de Kort <rob@studio1902.nl>
2026-01-27 09:06:41 +0100

    Finish post install setMailFromAddress

--- StarterKitPostInstall.php
+++ StarterKitPostInstall.php
@@ -198,6 +198,9 @@ class StarterKitPostInstall
         );
 
         $this->replaceInEnv('MAIL_FROM_ADDRESS="hello@example.com"', "MAIL_FROM_ADDRESS=\"{$email}\"");
+        $this->replaceInReadme('MAIL_FROM_ADDRESS="hello@example.com"', "MAIL_FROM_ADDRESS=\"{$email}\"");
+        $this->replaceInContact('to: info@site.com', "to: {$email}.com");
+        $this->replaceInContact('reply_to: info@site.com', "reply_to: {$email}.com");
     }
 
     protected function runPeakClearSite(): void
